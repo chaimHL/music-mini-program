ebadaf5c3ceca5541a54f1ea5b1339221a132cd0
chaimHL <1302906470@qq.com>
2024-08-14 17:04:23 +0800

    *新增：音乐播放页的切换

--- miniprogram/pages/music-play/music-play.ts
+++ miniprogram/pages/music-play/music-play.ts
@@ -1,15 +1,22 @@
 // pages/music-play/music-play.ts
 import song from '../../services/requsets/song'
+const app = getApp()
 Page({
   data: {
     songData: {},
-    lrc: ''
+    lrc: '',
+    current: 0, // 当前激活的 swiperItem
+    contentHeight: 667 // 内容区域高度
   },
   onLoad(options: any) {
+    // 计算内容区域高度
+    const { screenHeight, statusBarHeight } = app.globalData.windowInfo
+    const contentHeight = screenHeight - statusBarHeight - 44
+    this.setData({ contentHeight })
+
     const { id } = options
     // 获取歌曲详情
     song.detail(id).then(res => {
-      console.log(res)
       this.setData({
         songData: res.songs[0]
       })
@@ -19,6 +26,11 @@ Page({
       this.setData({
         lrc: res.lrc.lyric
       })
+    })
+  },
+  onSwiperChange(event: WechatMiniprogram.SwiperChange) {
+    this.setData({
+      current: event.detail.current
     })
   }
 })
